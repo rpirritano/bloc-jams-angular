134793972d3c94789025e6f111ba6ebc2e7b8d04
rpirritano <rpirritano@gmail.com>
2017-01-21 07:33:35 -0800

    Checkpoint 4 templates: completed

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,20 +1,20 @@
 (function() {
-     function config($stateProvider, $locationProvider) {
-         $locationProvider
-             .html5Mode({
-             enabled: true,
-             requireBase: false
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+               enabled: true,
+               requireBase: false
          });
          
         $stateProvider
             .state('landing', {
-            url: '/',
-            templateUrl: '/templates/landing.html'
+                url: '/',
+                templateUrl: '/templates/landing.html'
             })
             
             .state('album', {
-            url: '/album',
-            templateUrl: '/templates/album.html'
+                url: '/album',
+                templateUrl: '/templates/album.html'
             })
             
             .state('collection', {
