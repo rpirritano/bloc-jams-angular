623e01026420e4d6aeb84ae8439f281226acdffa
rpirritano <rpirritano@gmail.com>
2017-01-22 13:23:49 -0800

    Assignment 7 services: completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,6 +1,6 @@
 (function() {
     function SongPlayer() {
-        var SongPlayer = {};
+        var SongPlayer = {}; //SongPlayer variable is created and set to an empty object
         
         var currentSong = null;
         var currentBuzzObject = null;
@@ -29,11 +29,35 @@
         currentSong = song;
     };
         
+        /**
+        * @function playSong
+        * @desc plays the song (currentBuzzObject) and sets the song.playing to true
+        * @param {object} song
+        */
+        var playSong = function(song) {
+            currentBuzzObject.play();                                   
+            song.playing = true;
+        };
+        
+        /**
+        *Here the play method takes an argument, song, which is grabbed from album view 
+        *when a user clicks the play button. The ng-repeat directive used in album template
+        *will dictate which song to pass into the function. The play method creates a new
+        *buzz object using the songs audioUrl property and then calls Buzz's own play method
+        *on the object. To trigger the SongPlayer.play method an ngClick directive must be
+        *added to the play button anchor tag in album.html
+        */
+        /**
+        * @function SongPlayer.play
+        * @desc public method used to play song (Currently used in the album.html template) 
+        * @param {object} song
+        */
+        
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
+                
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
                     currentBuzzObject.play();
@@ -44,7 +68,19 @@
         return SongPlayer;
 }
  
+    //to use SongPlayer service, must be injected as a dependency. Music will
+    //be played from the album view, so it is injected in the AlbumCtrl
+    
     angular
         .module('blocJams')
         .factory('SongPlayer', SongPlayer);
 })();
+
+
+/**
+*the SongPlayer service contains:
+* 1 private attribute: currentBuzzObject
+* 1 public attribute: SongPlayer.currentSong
+* 1 private function: setSong
+* and two public methods: SongPlayer.play and SongPlayer.pause
+*/
