dc0bddaadc9a9b72f9cb11e5672a4a0d76b3e8b5
rpirritano <rpirritano@gmail.com>
2017-01-24 07:38:16 -0800

    fixing errors

---
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,20 @@
+(function() {
+         function AlbumCtrl(Fixtures, SongPlayer) {
+             this.albumData = Fixtures.getAlbum();
+             this.songPlayer = SongPlayer;
+     }
+    
+    angular
+        .module('blocJams')
+        .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
+})();
+
+
+
+
+
+
+
+
+
+ 
