773a5519004dacbbd66489e4e4c72fb4c9110017
rpirritano <rpirritano@gmail.com>
2017-01-24 23:37:06 -0800

    refactored player

---
+++ dist/scripts/controllers/CollectionCtrl 2.js
@@ -0,0 +1,16 @@
+ (function() {
+     function CollectionCtrl(Fixtures) {
+         this.albums = Fixtures.getCollection(12);
+     }
+ 
+     angular
+         .module('blocJams')
+         .controller('CollectionCtrl', ['Fixtures', CollectionCtrl]);
+ })();
+
+
+
+
+
+
+
