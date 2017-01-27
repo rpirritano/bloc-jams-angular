1f5c3b859af885259103e484fea7ec7f2dca04f2
rpirritano <rpirritano@gmail.com>
2017-01-26 23:50:06 -0800

    Checkpoint 8 services: completed

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,56 +1,156 @@
 (function() {
-    function SongPlayer() {
-        var SongPlayer = {};
+    function SongPlayer($rootScope, Fixtures) {
         
-        var currentSong = null;
-        var currentBuzzObject = null;
-    
-/**
-* @function setSong
-* @desc Stops currently playing song and loads new audio file as currentBuzzObject
-* @param {Object} song
-*/
-            
-    var setSong = function(song) {
-        if (currentBuzzObject) {
-            currentBuzzObject.stop();
-            currentSong.playing = null;
-        }
- 
+        //SongPlayer variable is created and set to an empty object
+        var SongPlayer = {};                                        
+        
+      
+        var currentAlbum = Fixtures.getAlbum();
+        
+        /**
+        * @function getSongIndex
+        * @desc determines the index of the current song in the current album 
+        * @param {object} song
+        */
+        var getSongIndex = function(song) {
+            return currentAlbum.songs.indexOf(song);
+        };
+        
+        /**
+        * @desc song object from songs list in album. Public attribute so that it can be used in the player bar
+        * @type {object} 
+        */
+        SongPlayer.currentSong = null;
+  
+      
+        
         /**
         * @desc Buzz object audio file
-        * @type {Object}
+        * @type {object}
         */
-        currentBuzzObject = new buzz.sound(song.audioUrl, {
-            formats: ['mp3'],
-            preload: true
-        });
- 
-        currentSong = song;
-    };
+        var currentBuzzObject = null;
         
-        SongPlayer.play = function(song) {
-            if (currentSong !== song) {
-                setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
-            } else if (currentSong === song) {
+        /**
+        * @function setSong
+        * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+        * @param {Object} song
+        */
+        var setSong = function(song) {
+            if (currentBuzzObject){                                  
+                currentBuzzObject.stop();                            
+                SongPlayer.currentSong.playing = null;                                                                                     
+               }                                                        
+           
+            currentBuzzObject = new buzz.sound(song.audioUrl, {     
+               formats: ['mp3'],                                         
+                preload: true                                           
+            });                     
+            
+
+            
+            SongPlayer.currentSong = song;
+        };
+        
+        /**
+        * @function playSong
+        * @desc plays the song (currentBuzzObject) and sets the song.playing to true
+        * @param {object} song
+        */
+        var playSong = function(song) {
+            currentBuzzObject.play();                                   
+            song.playing = true;
+        }; 
+        
+        /**
+        * @function stopSong
+        * @desc stops the song (currentBuzzObject) and sets the song.playing to null
+        * @param {object} song
+        */
+        var stopSong = function(song) {
+            currentBuzzObject.stop();
+            song.playing = null;
+        
+        };
+        
+  
+        /**
+        * @function SongPlayer.play
+        * @desc public method used to play song (Currently used in the album.html template) 
+        * @param {object} song
+        */
+        SongPlayer.play = function(song) {                              
+            song = song || SongPlayer.currentSong;
+            if (SongPlayer.currentSong !== song){
+               setSong(song);
+               playSong(song);
+                if (SongPlayer.muted === true) {
+                    SongPlayer.mute();
+                }
+                                                                        
+            } else if (SongPlayer.currentSong === song) {      
                 if (currentBuzzObject.isPaused()) {
                     currentBuzzObject.play();
-                }  
+                    if (SongPlayer.muted === true) {
+                    SongPlayer.mute();
+                }
+                }
+            }
+        }
+        
+        /**
+        * @function SongPlayer.pause
+        * @desc public method used to pause the current song (Currently used in the album.html template) 
+        * @param {object} song
+        */
+        SongPlayer.pause = function(song) {
+            song = song || SongPlayer.currentSong;
+            currentBuzzObject.pause();
+            song.playing = false;
+        }
+        
+        
+        /**
+        * @function SongPlayer.previous
+        * @desc function for the previous button on the player bar. Determines current song index,
+        * decrements by one, and then plays the song at that index (or stops if less than zero)
+        */
+        SongPlayer.previous = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex--;
+            
+            if (currentSongIndex < 0) {
+                stopSong(SongPlayer.currentSong);
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
             }
         };
         
-        SongPlayer.pause = function(song) {
-            currentBuzzObject.pause();
-            song.playing = false;
-        };
         
-        
-        return SongPlayer;
-}
- 
+        /**The service returns this object, making its 
+        *properties and methods public to the rest of the application 
+        */
+        return SongPlayer;                                            
+    }                                                               
+                                                                
+    
+    
+    //to use SongPlayer service, must be injected as a dependency. Music will
+    //be played from the album view, so it is injected in the AlbumCtrl
     angular
         .module('blocJams')
-        .factory('SongPlayer', SongPlayer);
+        .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);                         
 })();
+
+
+/**
+*the SongPlayer service contains:
+* 1 private attribute: currentBuzzObject
+* 1 public attribute: SongPlayer.currentSong
+* 1 private function: setSong
+* and two public methods: SongPlayer.play and SongPlayer.pause
+*/
+
+
+
