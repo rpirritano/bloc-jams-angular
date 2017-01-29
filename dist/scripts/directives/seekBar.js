f6b4a7445c27806bd50ec673244f675370dc8725
rpirritano <rpirritano@gmail.com>
2017-01-29 07:33:11 -0800

    Checkpoint 9 directives #1: completed

--- dist/scripts/directives/seekBar.js
+++ dist/scripts/directives/seekBar.js
@@ -3,6 +3,14 @@ For directives, the callback function (in this case, seekBar) is a factory funct
 */
 (function() {
      function seekBar($document) {
+
+         
+/**
+* @function calculatePercent
+* @desc calculates the horizontal percent along the seek bar where the event (passed
+* in from the view as $event) occured.
+* @param seekBar (directive, element in this case) and an event  
+*/         
          
 var calculatePercent = function(seekBar, event) {
     var offsetX = event.pageX - seekBar.offset().left;
@@ -20,11 +28,28 @@ var calculatePercent = function(seekBar, event) {
              scope: { },
              link: function(scope, element, attributes) {
              // directive logic to return
+                 
+                 /**
+                * @desc holds value of the seek bar(i.e. current playing song time/current song volume)
+                * @type number
+                */
                  scope.value = 0;
+                 
+                /**
+                * @desc Holds the element that matches the directive (<seek-bar>) as a jQuery object
+                * so jQuery methods can be called on it.
+                * @type directive
+                */
                  scope.max = 100;
                  
                  var seekBar = $(element);
  
+                 
+                /**
+                * @function percentString
+                * @desc calculates a percent based on the value and max value of a seek bar
+                */
+                 
                  var percentString = function () {
                      var value = scope.value;
                      var max = scope.max;
@@ -32,15 +57,37 @@ var calculatePercent = function(seekBar, event) {
                      return percent + "%";
                  };
                  
+                /**
+                * @function scope.fillStyle
+                * @desc returns the width of the seek bar fill element based on calculated percent
+                */
+                 
                  scope.fillStyle = function() {
                      return {width: percentString()};
                  };
                  
-                scope.onClickSeekBar = function(event) {
+                
+                 scope.thumbStyle = function(){
+                     return {left: percentString()};     
+                 };
+                 
+                /**
+                * @function scope.onClickSeekBar
+                * @desc updates the seek bar value based on the seekbars width and the location of 
+                * the users click on the seek bar 
+                * @param event
+                */
+                 
+                 scope.onClickSeekBar = function(event) {
                     var percent = calculatePercent(seekBar, event);
                     scope.value = percent * scope.max;
                 };
                  
+                /**
+                * @function scope.trackThumb
+                * @desc similar to scope.onClickSeekBar, but uses $apply to constantly apply the change
+                * in value of scope.value as the user drags the seek bar thumb
+                */
                  scope.trackThumb = function() {
                      $document.bind('mousemove.thumb', function(event) {
                          var percent = calculatePercent(seekBar, event);
